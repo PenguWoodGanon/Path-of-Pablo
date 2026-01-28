3ed6e5f9eac58215321ab2099ba94779b85763c3
PenguWoodGanon <maxwellj2022@icloud.com>
2026-01-28 13:48:46 -0700

    Update tower_of_grass.gd

--- Godot - Path of Pablo/Scripts/tower_of_grass.gd
+++ Godot - Path of Pablo/Scripts/tower_of_grass.gd
@@ -5,6 +5,6 @@ extends Node2D
 
 func _on_door_level_1_body_entered(body: PhysicsBody2D):
 	print("FAAAAA")
-	if Input.is_action_just_pressed("click"):
+	if Input.is_action_just_pressed(""):
 		print("works")
 		body.set_position($"Level 1/Door_Level_1/Marker2D".global_position)
