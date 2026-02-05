6f14b58c8a41db871f0a5c40b06e5b27aa85319b
PenguWoodGanon <maxwellj2022@icloud.com>
2026-02-05 11:06:36 -0700

    Added Dialogue with Mr. Smiley

--- Godot - Path of Pablo/Scripts/dormant_grasslands.gd
+++ Godot - Path of Pablo/Scripts/dormant_grasslands.gd
@@ -2,17 +2,16 @@ extends Node2D
 
 var talked = false
 var numtalk = 0
+var mr_smiley_pressed = false
 @onready var mr_smiley_talk_panel = $"Global_Characters/Mr Smiley Talk Panel"
 @onready var mr_smiley = $"MrSmiley Event/MrSmiley"
 @onready var label = $"MrSmiley Event/Label"
 @onready var mr_smiley_button = $"MrSmiley Event/Button"
 @onready var hud = $Global_Characters/HUD
+@onready var text_1 = $"Global_Characters/Mr Smiley Talk Panel/Panel/text1"
+@onready var text_2 = $"Global_Characters/Mr Smiley Talk Panel/Panel/text2"
+@onready var text_3 = $"Global_Characters/Mr Smiley Talk Panel/Panel/text3"
 
-var text1: String = "Hello, I'm Mr. Smiley!"
-var text2: String = "Would you like me to help
-you on this adventure?"
-var text3: String = "Great!"
-var text4: String = "(Press c to open your character list)"
 
 func _ready():
 	mr_smiley_talk_panel.visible = false
@@ -20,17 +19,34 @@ func _ready():
 	label.visible = true
 	mr_smiley_button.visible = true
 	hud.visible = true
+	text_1.visible = false
+	text_2.visible = false
+	text_3.visible = false
 
 func _process(_delta):
-	if talked == true:
-		if numtalk == 0:
-			print("works")
-			label.text = text1
+	if numtalk == 0 and mr_smiley_pressed == true:
+		text_1.visible = true
+		text_2.visible = false
+		text_3.visible = false
+	if numtalk == 1 and mr_smiley_pressed == true:
+		text_1.visible = false
+		text_2.visible = true
+		text_3.visible = false
+	if numtalk == 2 and mr_smiley_pressed == true:
+		text_1.visible = false
+		text_2.visible = false
+		text_3.visible = true
+	if numtalk == 3 and mr_smiley_pressed:
+		text_1.visible = false
+		text_2.visible = false
+		text_3.visible = false
+		mr_smiley_talk_panel.visible = false
+		Global.char_move = true
 
 func _on_mr_smiley_pressed():
+	mr_smiley_pressed = true
 	if talked == false:
 		mr_smiley_talk()
-	print("works")
 
 func mr_smiley_talk():
 	talked = true
