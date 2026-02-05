b78f68a9b7fcf7808307671cc573b77e88019510
PenguWoodGanon <maxwellj2022@icloud.com>
2026-02-05 10:53:48 -0700

    updated things

--- Godot - Path of Pablo/Scripts/dormant_grasslands.gd
+++ Godot - Path of Pablo/Scripts/dormant_grasslands.gd
@@ -1,13 +1,18 @@
 extends Node2D
 
 var talked = false
+var numtalk = 0
 @onready var mr_smiley_talk_panel = $"Global_Characters/Mr Smiley Talk Panel"
 @onready var mr_smiley = $"MrSmiley Event/MrSmiley"
 @onready var label = $"MrSmiley Event/Label"
 @onready var mr_smiley_button = $"MrSmiley Event/Button"
 @onready var hud = $Global_Characters/HUD
 
-
+var text1: String = "Hello, I'm Mr. Smiley!"
+var text2: String = "Would you like me to help
+you on this adventure?"
+var text3: String = "Great!"
+var text4: String = "(Press c to open your character list)"
 
 func _ready():
 	mr_smiley_talk_panel.visible = false
@@ -15,6 +20,12 @@ func _ready():
 	label.visible = true
 	mr_smiley_button.visible = true
 	hud.visible = true
+
+func _process(_delta):
+	if talked == true:
+		if numtalk == 0:
+			print("works")
+			label.text = text1
 
 func _on_mr_smiley_pressed():
 	if talked == false:
@@ -29,3 +40,7 @@ func mr_smiley_talk():
 	mr_smiley_button.visible = false
 	hud.visible = false
 	Global.char_move = false
+
+
+func _on_next_pressed():
+	numtalk += 1
