68135e693d6ca048b8a2cf698f50e6fa22248fa6
PenguWoodGanon <maxwellj2022@icloud.com>
2026-02-24 07:29:45 -0700

    Edited the Booster Pack and Character Selection

--- Godot - Path of Pablo/Scripts/characters_select.gd
+++ Godot - Path of Pablo/Scripts/characters_select.gd
@@ -1,106 +1,101 @@
 extends Panel
 
 @onready var mrsmiley_locked = $MrSmileyCard/LOCKED
+@onready var donut_locked = $DonutCard/LOCKED2
+@onready var warrior_locked = $WarriorCard/LOCKED3
+@onready var megapablo_locked = $MegaPabloCard/LOCKED4
+@onready var speed_locked = $SpeedCard/LOCKED5
+@onready var paul_locked = $PaulCard/LOCKED6
+@onready var slime_locked = $SlimeCard/LOCKED7
 
 
 func _ready():
-	mrsmiley_locked.visible = true
+	update_locks()
 
 func _process(_delta):
-	if Global.mrsmiley_unlocked == true:
-		mrsmiley_locked.visible = false
+	update_locks()
 
-#Mr. Smiley Button
+# -------------------------
+# LOCK VISIBILITY
+# -------------------------
+func update_locks():
+	mrsmiley_locked.visible = !Global.mrsmiley_unlocked
+	donut_locked.visible = !Global.donut_unlocked
+	warrior_locked.visible = !Global.knight_unlocked
+	megapablo_locked.visible = !Global.megapablo_unlocked
+	speed_locked.visible = !Global.speed_unlocked
+	paul_locked.visible = !Global.paul_unlocked
+	slime_locked.visible = !Global.slime_unlocked
+
+
+# -------------------------
+# CHARACTER RESET
+# -------------------------
+func reset_characters():
+	Global.char_pablo = false
+	Global.char_mrsmiley = false
+	Global.char_speed = false
+	Global.char_megapablo = false
+	Global.char_knight = false
+	Global.char_paul = false
+	Global.char_slime = false
+	Global.char_donut = false
+
+
+# -------------------------
+# BUTTONS
+# -------------------------
+
+# Mr. Smiley
 func _on_button_pressed():
-	if Global.mrsmiley_unlocked == true:
-		Global.char_pablo = false
+	if Global.mrsmiley_unlocked:
+		reset_characters()
 		Global.char_mrsmiley = true
-		Global.char_speed = false
-		Global.char_megapablo = false
-		Global.char_knight = false
-		Global.char_paul = false
-		Global.char_slime = false
-		Global.char_donut = false
 
-#Pablo Button
+
+# Pablo
 func _on_pablo_pressed():
-		Global.char_pablo = true
-		Global.char_mrsmiley = false
-		Global.char_speed = false
-		Global.char_megapablo = false
-		Global.char_knight = false
-		Global.char_paul = false
-		Global.char_slime = false
-		Global.char_donut = false
+	reset_characters()
+	Global.char_pablo = true
 
-#Speed Button
+
+# Speed
 func _on_speed_pressed():
-	if Global.speed_unlocked == true:
-		Global.char_pablo = false
-		Global.char_mrsmiley = false
-		Global.char_speed = false
-		Global.char_megapablo = false
-		Global.char_knight = false
-		Global.char_paul = false
-		Global.char_slime = false
-		Global.char_donut = false
+	if Global.speed_unlocked:
+		reset_characters()
+		Global.char_speed = true
 
-#Megapablo Button
+
+# Mega Pablo
 func _on_megapablo_pressed():
-	if Global.megapablo_unlocked == true:
-		Global.char_pablo = false
-		Global.char_mrsmiley = false
-		Global.char_speed = false
+	if Global.megapablo_unlocked:
+		reset_characters()
 		Global.char_megapablo = true
-		Global.char_knight = false
-		Global.char_paul = false
-		Global.char_slime = false
-		Global.char_donut = false
 
-#Warrior Button
+
+# Warrior
 func _on_warrior_pressed():
-	if Global.knight_unlocked == true:
-		Global.char_pablo = false
-		Global.char_mrsmiley = false
-		Global.char_speed = false
-		Global.char_megapablo = false
+	if Global.knight_unlocked:
+		reset_characters()
 		Global.char_knight = true
-		Global.char_paul = false
-		Global.char_slime = false
-		Global.char_donut = false
 
-#Paul Pressed
+
+# Paul
 func _on_paul_pressed():
-	if Global.paul_unlocked == true:
-		Global.char_pablo = false
-		Global.char_mrsmiley = false
-		Global.char_speed = false
-		Global.char_megapablo = false
-		Global.char_knight = false
+	if Global.paul_unlocked:
+		reset_characters()
 		Global.char_paul = true
-		Global.char_slime = false
-		Global.char_donut = false
 
-#Slime Button
+
+# Slime
 func _on_slime_pressed():
-	if Global.slime_unlocked == true:
-		Global.char_pablo = false
-		Global.char_mrsmiley = false
-		Global.char_speed = false
-		Global.char_megapablo = false
-		Global.char_knight = false
-		Global.char_paul = false
+	if Global.slime_unlocked:
+		reset_characters()
 		Global.char_slime = true
-		Global.char_donut = false
 
-#Donut Button
+
+# Donut
 func _on_donut_pressed():
-	if Global.donut_unlocked == true:
-		Global.char_pablo = false
-		Global.char_mrsmiley = false
-		Global.char_speed = false
-		Global.char_megapablo = false
-		Global.char_knight = false
-		Global.char_paul = false
-		Global.char_slime = false
+	if Global.donut_unlocked:
+		reset_characters()
 		Global.char_donut = true
--- Godot - Path of Pablo/booster_pack.gd
+++ Godot - Path of Pablo/booster_pack.gd
@@ -5,9 +5,8 @@ extends Panel
 @onready var anim_player = $BoosterPack/AnimationPlayer
 
 var random_num = RandomNumberGenerator.new()
-var spun = false
 
-#Apperance vars
+# Appearance vars
 @onready var mr_smiley_card = $Cards/MrSmileyCard
 @onready var speed_card = $Cards/SpeedCard
 @onready var paul_card = $Cards/PaulCard
@@ -15,13 +14,16 @@ var spun = false
 @onready var william_card = $Cards/WilliamCard
 @onready var donut_card = $Cards/DonutCard
 @onready var mega_pablo_card = $Cards/MegaPabloCard
-
 @onready var timer = $Cards/Timer
 
+var testing = false
+
 func _ready():
+	random_num.randomize()
+
 	cost_panel.visible = true
 	title_panel.visible = true
-	mega_pablo_card.visible = false
+
 	mr_smiley_card.visible = false
 	speed_card.visible = false
 	paul_card.visible = false
@@ -29,97 +31,90 @@ func _ready():
 	william_card.visible = false
 	donut_card.visible = false
 	mega_pablo_card.visible = false
-	Global.char_mrsmiley = false
 
 func _process(_delta):
-	#if Input.is_action_just_pressed("testing"):
-		#print("testing")
-		#roll()
-		pass
+	if Input.is_action_just_pressed("testing"):
+		if testing == true:
+			cost_panel.visible = false
+			title_panel.visible = false
+			await roll()
+
+func reset():
+	cost_panel.visible = true
+	title_panel.visible = true
+	random_num.randomize()
 
 func _on_button_pressed():
 	if Global.coins >= 15:
 		Global.coins -= 15
 		cost_panel.visible = false
 		title_panel.visible = false
-		roll()
+		await roll()
+
 
 func roll():
-	anim_player.play("spin")
-	anim_player.play("spin_fast")
 	anim_player.play("spin_faster")
+
 	random_num.randomize()
 	var random_int = random_num.randi_range(2, 8)
-	print(random_int)
-	if random_int == 2:
-		#Mr.Smiley
-		if Global.mrsmiley_unlocked == false:
-			Global.mrsmiley_unlocked = true
-			mr_smiley_card.visible = true
-			timer.start()
-			await timer.timeout
-			mr_smiley_card.visible = false
-		else:
-			roll()
-	elif random_int == 3:
-		#Speed
-		if Global.speed_unlocked == false:
-			Global.speed_unlocked = true
-			speed_card.visible = true
-			timer.start()
-			await timer.timeout
-			speed_card.visible = false
-		else:
-			roll()
-	elif random_int == 4:
-		#Paul
-		if Global.paul_unlocked == false:
-			Global.paul_unlocked = true
-			paul_card.visible = true
-			timer.start()
-			await timer.timeout
-			paul_card.visible = false
-		else:
-			roll()
-	elif random_int == 5:
-		#Slime
-		if Global.slime_unlocked == false:
-			Global.slime_unlocked = true
-			slime_card.visible = true
-			timer.start()
-			await timer.timeout
-			slime_card.visible = false
-		else:
-			roll()
+	print("Rolled:", random_int)
 
-	if random_int == 6:
-		if Global.knight_unlocked == false:
-			Global.knight_unlocked = true
-			william_card.visible = true
-			timer.start()
-			await timer.timeout
-			william_card.visible = false
-		else:
-			roll()
+	if random_int == 2 and !Global.mrsmiley_unlocked:
+		Global.mrsmiley_unlocked = true
+		mr_smiley_card.visible = true
+		await get_tree().create_timer(2.0).timeout
+		mr_smiley_card.visible = false
+		reset()
 
-	if random_int == 7:
-		#Donut
-		if Global.donut_unlocked == false:
-			Global.donut_unlocked = true
-			donut_card.visible = true
-			timer.start()
-			await timer.timeout
-			donut_card.visible = false
-		else:
-			roll()
+	elif random_int == 3 and !Global.speed_unlocked:
+		Global.speed_unlocked = true
+		speed_card.visible = true
+		await get_tree().create_timer(2.0).timeout
+		speed_card.visible = false
+		reset()
 
-	if random_int == 8:
-		#Mega Pablo
-		if Global.megapablo_unlocked == false:
-			Global.megapablo_unlocked = true
-			mega_pablo_card.visible = true
-			timer.start()
-			await timer.timeout
-			mega_pablo_card.visible = false
-		else:
-			roll()
+	elif random_int == 4 and !Global.paul_unlocked:
+		Global.paul_unlocked = true
+		paul_card.visible = true
+		await get_tree().create_timer(2.0).timeout
+		paul_card.visible = false
+		reset()
+
+	elif random_int == 5 and !Global.slime_unlocked:
+		Global.slime_unlocked = true
+		slime_card.visible = true
+		await get_tree().create_timer(2.0).timeout
+		slime_card.visible = false
+		reset()
+
+	elif random_int == 6 and !Global.knight_unlocked:
+		Global.knight_unlocked = true
+		william_card.visible = true
+		await get_tree().create_timer(2.0).timeout
+		william_card.visible = false
+		reset()
+
+	elif random_int == 7 and !Global.donut_unlocked:
+		Global.donut_unlocked = true
+		donut_card.visible = true
+		await get_tree().create_timer(2.0).timeout
+		donut_card.visible = false
+		reset()
+
+	elif random_int == 8 and !Global.megapablo_unlocked:
+		Global.megapablo_unlocked = true
+		mega_pablo_card.visible = true
+		await get_tree().create_timer(2.0).timeout
+		mega_pablo_card.visible = false
+		reset()
+
+	else:
+		print("Already unlocked, roll again")
+		roll()
+
+
+func show_card(card):
+	card.visible = true
+	timer.start()
+	await timer.timeout
+	card.visible = false
